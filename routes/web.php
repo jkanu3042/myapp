309f94b9dd441b2f9fda5a34bf8ac56d774bed40
jkanu3042 <jkanu3042@naver.com>
2017-08-17 15:37:26 +0900

    'Composer'

--- routes/web.php
+++ routes/web.php
@@ -74,16 +74,69 @@ Route::get('/home', 'HomeController@index')->name('home');
 //    var_dump($article->toArray());
 //});
 
-Route::get('mail', function() {
-   $article = \App\Article::with('user')->find(1);
+//Route::get('mail', function() {
+//   $article = \App\Article::with('user')->find(1);
+//
+//    return Mail::send(
+//        'emails.articles.created',
+//        compact('article'),
+//        function($message) use ($article) {
+//            $message->to('cshmp6@gmail.com');
+//            $message->subject('새 글이 등록되었습니다.'.$article->title);
+//        }
+//    );
+//
+//});
 
-    return Mail::send(
-        'emails.articles.created',
-        compact('article'),
-        function($message) use ($article) {
-            $message->to('cshmp6@gmail.com');
-            $message->subject('새 글이 등록되었습니다.'.$article->title);
-        }
-    );
+Route::get('markdown', function(){
+   $text =<<<EOT
+# 마크다운 예제 1
+
+이 문서는 [마크다운][1]으로 썼습니다. 화면에서는 HTML로 변환되어있습니다.
+
+## 순서 없는 목록
+
+- 첫 번째 항목
+- 두 번째 항복[^1]
+
+[1] : http://daringfireball.net/projects/markdown
+
+[^1] : 두번째 항목_ http://google.com
+
+
+
+EOT;
+
+    return app(ParsedownExtra::class)->text($text);
+
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
