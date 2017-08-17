706073eb7b136236346582ddc1c758a60823ca9c
jkanu3042 <jkanu3042@naver.com>
2017-08-18 04:07:24 +0900

    '컨트롤러와_도우미_함수'

--- routes/web.php
+++ routes/web.php
@@ -64,12 +64,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::get('docs/{file?}', function ($file = null){
-    $text = (new App\Documentation)->get($file);
-
-    return app(ParsedownExtra::class)->text($text);
-
-});
+Route::get('docs/{file?}', 'DocsController@show');
 
 
 
